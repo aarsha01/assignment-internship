da62ff5e11feaa458881f0a51009d10cff91bd2c
jazzirHussain <jasirhussain2002@gmail.com>
2023-11-03 17:51:29 +0530

    changes

--- src/assets/data.js
+++ src/assets/data.js
@@ -7,13 +7,13 @@ import innocence from './innocence.jpg'
 import lovingyourself from './loving yourself.jpg'
 
 const productData = [
-    { id: 1, title: 'women', imageUrl: women, price:'$300'},
-    { id: 2, title: 'Loving yourself', imageUrl: lovingyourself, price:'$250' },
-    { id: 3, title: 'Dream', imageUrl: dream , price:'$300' },
-    { id: 4, title: 'Innocence', imageUrl: innocence,  price:'$250' },
-    { id: 5, title: 'Growing', imageUrl: growing,  price:'$250' },
-    { id: 6, title: 'Athousand words', imageUrl: words,  price:'$250' },
-    { id: 7, title: 'Chain within the head', imageUrl: chain,  price:'$250' },
+    { id: 1, title: 'women', imageUrl: women, price:300},
+    { id: 2, title: 'Loving yourself', imageUrl: lovingyourself, price:250 },
+    { id: 3, title: 'Dream', imageUrl: dream , price:300 },
+    { id: 4, title: 'Innocence', imageUrl: innocence,  price:250 },
+    { id: 5, title: 'Growing', imageUrl: growing,  price:250 },
+    { id: 6, title: 'Athousand words', imageUrl: words,  price:250 },
+    { id: 7, title: 'Chain within the head', imageUrl: chain,  price:250 },
     
 
   ];
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import './productPage.css'
 import productData from '../assets/data';
 import Item from '../components/Items';
-const Product=()=>{
-  return(
+import Filters from "../components/Filters";
+const Product = () => {
+
+  const [filters, setFilters] = useState({
+    price: 0
+  })
+  const [products, setProducts] = useState(productData)
+
+  const handleFilterChange = (key, val) => {
+    setFilters({ ...filters, [key]: val })
+  }
+
+  const applyFilter = () => {
+    let prds = productData.filter(obj => {
+      console.log(obj.price, filters.price);
+      return obj.price <= filters.price
+    })
+    console.log(prds);
+    setProducts(prds)
+  }
+  return (
     <div className="products">
       <h1>POPULAR</h1>
       <hr />
+      <Filters
+        handleFilterChange={handleFilterChange}
+        value={filters}
+        applyFilter={applyFilter}
+      />
       <div className="product-item">
-       {
-        productData.map((value, i)=>{
-          return <Item  title={value.title} imageUrl={value.imageUrl} price={value.price}/>
-        })
-       }
+        {products.length > 0 ?
+          products.map((value, i) => {
+            return <Item title={value.title} imageUrl={value.imageUrl} price={value.price} />
+          })
+          :
+          <p className="err-msg">Uh oh. no matches. try other filters :)</p>
+        }
       </div>
 
     </div>
