b4251304a293c6cc08190598b601114f095b3fcf
Aarsha <malu22leena@gmail.com>
2023-11-03 18:34:35 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
-
+import About from './pages/about';
 
 
 
@@ -15,6 +15,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />}/>
         
       </Routes>
     </div>
---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Product from './Product'
+const Home=()=>{
+  return(
+    <div>
+    <p>Details of the wesite</p>
+    </div>
+  )
+}
+export default Home
